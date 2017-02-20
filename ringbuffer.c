73b089bd0c23ebea2d41fdc6a367d32911962763
Adam Dorling <adam.x.dorling@gmail.com>
2017-02-20 00:21:33 +0000

    added ring buffer:x

---
+++ ringbuffer.c
@@ -0,0 +1,86 @@
+#include <stdlib.h>
+#include <assert.h>
+#include "ringbuffer.h"
+
+struct ringbuffer_t {
+    BUFFERTYPE *buffer, *head, *tail, *end;
+    size_t count;
+    size_t size;
+};
+
+ringbuffer_t ringbuffer_new(size_t size) {
+    ringbuffer_t rbuffer = NULL;
+
+    rbuffer = malloc(sizeof(struct ringbuffer_t));
+
+    assert(rbuffer != NULL);
+
+    rbuffer->size = size;
+    rbuffer->buffer = malloc((rbuffer->size) * sizeof(BUFFERTYPE));
+    assert(rbuffer->buffer);
+
+    rbuffer->head = rbuffer->tail = rbuffer->buffer;
+    rbuffer->count = 0;
+    rbuffer->end = rbuffer->buffer + rbuffer->size;
+
+    return rbuffer;
+};
+
+
+void ringbuffer_free(ringbuffer_t *buffer) {
+    assert(buffer && *buffer);
+    free((*buffer)->buffer);
+    free(*buffer);
+    *buffer = NULL;
+}
+
+size_t ringbuffer_size(ringbuffer_t buffer) {
+    return buffer->size;
+}
+
+BUFFERTYPE ringbuffer_head(ringbuffer_t buffer) {
+    return *buffer->head;
+}
+
+BUFFERTYPE ringbuffer_tail(ringbuffer_t buffer) {
+    return *buffer->tail;
+}
+
+size_t ringbuffer_count(ringbuffer_t buffer) {
+    return buffer->count;
+}
+
+int ringbuffer_push(ringbuffer_t buffer, BUFFERTYPE item) {
+    if (ringbuffer_is_full(buffer)) {
+        return 0;
+    }
+    *buffer->tail++ = item;
+    if(buffer->tail >= buffer->end){
+        buffer->tail = buffer->buffer;
+    }
+    buffer->count++;
+    return 1;
+}
+
+BUFFERTYPE ringbuffer_shift(ringbuffer_t buffer){
+    if(ringbuffer_is_empty(buffer))
+    {
+        return NULL;
+    }
+    buffer->count--;
+    BUFFERTYPE output = *buffer->head++;
+    if(buffer->head >= buffer->end){
+        buffer->head = buffer->buffer;
+    }
+    return output;
+}
+
+int ringbuffer_is_full(ringbuffer_t buffer){
+    return buffer->count == buffer->size;
+}
+
+int ringbuffer_is_empty(ringbuffer_t buffer){
+    return buffer->count == 0;
+}
+
+
---
+++ ringbuffer.h
@@ -0,0 +1,62 @@
+
+//
+// Created by Adam on 19/02/2017.
+//
+
+#ifndef RINGBUFFER_RING_BUFFER_H
+#define RINGBUFFER_RING_BUFFER_H
+
+// Allows us to change the buffer type easily
+#define BUFFERTYPE char
+
+typedef struct ringbuffer_t *ringbuffer_t;
+
+/*
+ * Create a new ring buffer that has the given size, (size is maximum number of BUFFERTYPE it can hold)
+ */
+ringbuffer_t ringbuffer_new(size_t size);
+/*
+ * Deletes a ringbuffer and frees up the memory it was using
+ */
+void ringbuffer_free(ringbuffer_t *buffer);
+
+
+/*
+ * retrieves the maximum size of the buffer
+ */
+size_t ringbuffer_size(ringbuffer_t buffer);
+/*
+ * retrieves the head of the buffer
+ */
+BUFFERTYPE ringbuffer_head(ringbuffer_t buffer);
+/*
+ * retrieves the tail of the buffer
+ */
+BUFFERTYPE ringbuffer_tail(ringbuffer_t buffer);
+/*
+ * Counts the actual number of items in the buffer
+ */
+size_t ringbuffer_count(ringbuffer_t buffer);
+
+
+/*
+ * Adds an item to the buffer
+ */
+int ringbuffer_push(ringbuffer_t buffer, BUFFERTYPE item);
+/*
+ * Removes an item form the buffer and returns it
+ */
+BUFFERTYPE ringbuffer_shift(ringbuffer_t buffer);
+
+
+/*
+ * Returns whether the buffer
+ */
+int ringbuffer_is_full(ringbuffer_t buffer);
+/*
+ * Returns whether the buffer is empty
+ */
+int ringbuffer_is_empty(ringbuffer_t buffer);
+
+
+#endif //RINGBUFFER_RING_BUFFER_H
